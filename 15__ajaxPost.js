3c2d740fc7e1037a17deb00a5098ac1f8858a509
Ahmed Faysale <ahmedfoysal0161@gmail.com>
2023-06-20 23:35:14 +0600

    edit 15__ajaxPost.js file

---
+++ 15__ajaxPost.js
@@ -0,0 +1,31 @@
+function sendPostRequest() {
+  // Prepare the JSON payload
+  var payload = {
+    name: "John Doe",
+    email: "johndoe@example.com",
+  };
+
+  // Set the AJAX request parameters
+  var requestOptions = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(payload),
+  };
+
+  // Send the POST request with the JSON payload
+  fetch("https://jsonplaceholder.typicode.com/users", requestOptions)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log("Request successful!");
+      console.log(data);
+    })
+    .catch((error) => {
+      console.log("Request failed with error:");
+      console.error(error);
+    });
+}
+
+// Call the function to send the AJAX POST request
+sendPostRequest();
